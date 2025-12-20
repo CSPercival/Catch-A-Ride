fb94207950c3d7b136a31bd37b783d4795a83d09
Cyryl Szatan <cszatan@LAPTOP-OMEN.localdomain>
2025-12-20 11:20:20 +0100

    initial commit

---
+++ initializer.py
@@ -0,0 +1,18 @@
+import zipfile
+import os
+import shutil
+
+def main():
+    # resources_to_unzip["GTFS.zip", "XML.zip"]
+    resources_path = "./Resources"
+    resources_zipped_path = "./Resources-zipped"
+
+    if(os.path.isdir(resources_path)):
+        shutil.rmtree(resources_path)
+
+    for item in os.listdir(resources_zipped_path):
+        with zipfile.ZipFile(resources_zipped_path + "/" + item, 'r') as zip_res:
+            zip_res.extractall(resources_path)
+
+if __name__ == "__main__":
+    main()
